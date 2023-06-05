b5ab2da93d4419f4c77d0059779217cc34db9b48
bdbai <bdbaiapp@163.com>
2023-06-06 00:55:49 +0800

    Fix resource table name

--- ytflow/src/data/migrations/V4__resource.sql
+++ ytflow/src/data/migrations/V4__resource.sql
@@ -10,7 +10,7 @@ CREATE TABLE `yt_resources` (
 
 CREATE TABLE `yt_resources_url` (
     `id` INTEGER PRIMARY KEY,
-    `resource_id` INTEGER NOT NULL UNIQUE REFERENCES `yt_plugin_resource`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    `resource_id` INTEGER NOT NULL UNIQUE REFERENCES `yt_resources`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
     `url` TEXT NOT NULL,
     `etag` VARCHAR(255),
     `last_modified` VARCHAR(255),
@@ -19,7 +19,7 @@ CREATE TABLE `yt_resources_url` (
 
 CREATE TABLE `yt_resources_github_release` (
     `id` INTEGER PRIMARY KEY,
-    `resource_id` INTEGER NOT NULL UNIQUE REFERENCES `yt_plugin_resource`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    `resource_id` INTEGER NOT NULL UNIQUE REFERENCES `yt_resources`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
     `github_username` VARCHAR(255) NOT NULL,
     `github_repo` VARCHAR(255) NOT NULL,
     `asset_name` VARCHAR(255) NOT NULL,
