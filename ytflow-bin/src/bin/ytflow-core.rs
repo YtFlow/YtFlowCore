b1952da03e2537271048df4fb8fcb48c3867fff0
bdbai <bdbaiapp@163.com>
2024-01-14 23:28:23 +0800

    fix bin entrypoint link kind on non-win platforms

--- ytflow-bin/src/bin/ytflow-core.rs
+++ ytflow-bin/src/bin/ytflow-core.rs
@@ -1,6 +1,6 @@
 fn main() {
     #[cfg_attr(windows, link(name = "ytflow_shared_lib", kind = "raw-dylib"))]
-    #[cfg_attr(not(windows), link(name = "ytflow_shared_lib", kind = "raw-dylib"))]
+    #[cfg_attr(not(windows), link(name = "ytflow_shared_lib", kind = "dylib"))]
     extern "C" {
         fn ytflow_bin_exec_main();
     }
